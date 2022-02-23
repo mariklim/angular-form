68118ac77057f704f993b25c2b09ae5a9cd32b53
mariklim <maria.klimenko889@gmail.com>
2022-02-23 10:00:31 +0100

    angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-form';
+
+  // Two way binding, in html <input type="text" [(ngModel)]="myInput">
+  myInput:string = "";
+
+
+
+  onSubmit(myForm : NgForm){
+    console.log(myForm)
+  }
+
+
+  // Template Driven Form 2
+
+  formObjDue:{name:string,surname:string}={
+    name:'',
+    surname: ''
+  }
+
+
+  onSubmit2(){
+    console.log(this.formObjDue)
+  }
 }
